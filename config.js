28037731704973da84d2024d81162f73870dd6c9
addelphin <106343339+addeplens@users.noreply.github.com>
2022-05-29 06:53:29 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.Prefix = `~`;
+exports.Token = `OTc3MDM3OTQxNDg0NDQ5Nzky.Gakeyv.GssCICFBBW7bZTkbiiLjKN3wAPfQRfl621okzo`;
+exports.Color = `RANDOM`;
+
+//Put the Token in Client secrets LOCK Button
